a8a0cc354b7762f8f084b620594cfee0090d7ee9
DsAtHuH <ds@huh.social>
2022-01-11 14:25:15 +0000

    :heavy_plus_sign: Add logo minting functionality.

--- deploy/00_deploy_whitepaper_nft copy.js
+++ deploy/00_deploy_whitepaper_nft copy.js
@@ -4,7 +4,7 @@ export default async ({ getNamedAccounts, deployments }) => {
   await deploy('ERC721PresetMinterPauserAutoId', {
     from: deployer,
     proxy: true,
-    args: ["HUH Nation Directive NFT","NFT", "https://whitepaper.huh.social"],
+    args: ["HUH Nation Directive NFT","HNDNFT", "https://whitepaper.huh.social"],
     log: true
   })
 }
